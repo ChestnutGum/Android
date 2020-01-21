3742e559477c9b859136cc9de42b4cdc55c4c181
ChestnutGum <kelly.boutemeur@isen.yncrea.fr>
2020-01-21 22:37:43 +0100

    Gérer l'authentification. Vérifier l'identifiant et le mot de passe.

--- app/src/main/java/fr/isen/kelly/androidtoolbox/MainActivity.kt
+++ app/src/main/java/fr/isen/kelly/androidtoolbox/MainActivity.kt
@@ -12,8 +12,7 @@ import androidx.core.app.ComponentActivity
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
+import android.view.View
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,43 +23,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //titre
         my_Title=findViewById(R.id.my_Title)
+        //on récupère l'identifiant
         my_Id=findViewById(R.id.my_Id)
+        //bouton valider
         myButtonValidate=findViewById(R.id.button_validate)
         //myButtonValidate.isEnabled=false //désactiver le bouton au lancement de l'application
         //puis l'activer lorsqu'il a saisi ses infos
+        //on récupère le mot de passe
         myPass=findViewById(R.id.myPass)
-        myButtonValidate.setOnClickListener {
-            // clearing user_name and password edit text views on reset button click
-            my_Id.setText("")
-            myPass.setText("")
-        }
+
+
+
+        //fonction de notre onClick
+        //vérification du mot de passe et de l'identifiant utilisateur
+        //fun login(view:View) {
+
+        //}
+
 
         // set on-click listener
         myButtonValidate.setOnClickListener {
-            val id = my_Id.text
-            val password = myPass.text
-            Toast.makeText(this@MainActivity, "you clicked", Toast.LENGTH_LONG).show()
-            val homeActivity = Intent(this@MainActivity, activity_home::class.java)
-            startActivity(homeActivity)
+
+            //afficher un message
+            if (my_Id.getText().toString().equals("admin") && myPass.getText().toString().equals("admin"))
+            {
+                //correct password
+                Toast.makeText(this@MainActivity, "Bienvenue !", Toast.LENGTH_LONG).show()
+                //passage à la page home
+                val homeActivity = Intent(this@MainActivity, activity_home::class.java)
+                startActivity(homeActivity)
+            }
+            else
+            {
+                //wrong password
+                Toast.makeText(this@MainActivity, "Try Again...", Toast.LENGTH_LONG).show()
+            }
+
         }
 
-        my_Id.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
-            }
-
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                // This is where we'll check the user input
-                //myButtonValidate.isEnabled = s.toString().length() != 0
-
-            }
-
-            override fun afterTextChanged(s: Editable) {
-
-            }
-
-        })
         //myButtonValidate.setOnClickListener(object : View.OnClickListener() {
             //fun onClick(v: View) {
                 // The user just clicked
