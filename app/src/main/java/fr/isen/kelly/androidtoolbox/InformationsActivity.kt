7bf4a4e9b943c66013bd0a6a17911e14910390e3
ChestnutGum <kelly.boutemeur@isen.yncrea.fr>
2020-01-28 17:32:20 +0100

    Choisir entre une image provenant de la galerie et une image prise par l'appareil photo. (Début)

--- app/src/main/java/fr/isen/kelly/androidtoolbox/InformationsActivity.kt
+++ app/src/main/java/fr/isen/kelly/androidtoolbox/InformationsActivity.kt
@@ -3,6 +3,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
@@ -21,12 +22,12 @@ import java.io.FileNotFoundException
 
 class InformationsActivity : AppCompatActivity() {
     private lateinit var myButtonPicture: ImageButton
-    val REQUEST_CODE:Int=1
+    val REQUEST_CODE: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_informations)
 
-        myButtonPicture=findViewById(R.id.buttonPicture)
+        myButtonPicture = findViewById(R.id.buttonPicture)
 
         //permissionRecyclerView.apply{
         //myList.layoutManager = LinearLayoutManager(this)
@@ -39,89 +40,94 @@ class InformationsActivity : AppCompatActivity() {
         //}
 
         myButtonPicture.setOnClickListener {
-            val photoPickerIntent=Intent(Intent.ACTION_PICK)
+            val photoPickerIntent = Intent(Intent.ACTION_PICK)
             photoPickerIntent.type = "image/*"
             startActivityForResult(photoPickerIntent, REQUEST_CODE)
         }
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS),PERMISSIONS_REQUEST_READ_CONTACT)
-            }
-        else{
-                displayContact()
-            }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                PERMISSIONS_REQUEST_READ_CONTACT
+            )
+        } else {
+            displayContact()
+        }
 
     }
 
-        private fun displayContact() {
-            val contacts = loadContacts()
-            myList.layoutManager = LinearLayoutManager(this)
-            myList.adapter = ContactsAdapter(loadContacts())
-            val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-            myList.addItemDecoration(itemDecoration)
-        }
+    private fun displayContact() {
+        val contacts = loadContacts()
+        myList.layoutManager = LinearLayoutManager(this)
+        myList.adapter = ContactsAdapter(loadContacts())
+        val itemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        myList.addItemDecoration(itemDecoration)
+    }
 
-        private fun loadContacts(): List<Contact> {
-            val contactNameList=arrayListOf<Contact>()
-            val phoneCursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null)
-            phoneCursor?.let { cursor ->
-                while (cursor.moveToNext()){
-                    val name=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                    contactNameList.add(Contact(name, true))
-                }
-                cursor.close()
+    private fun loadContacts(): List<Contact> {
+        val contactNameList = arrayListOf<Contact>()
+        val phoneCursor =
+            contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null)
+        phoneCursor?.let { cursor ->
+            while (cursor.moveToNext()) {
+                val name =
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                contactNameList.add(Contact(name, true))
             }
-
-            return contactNameList
-        }
-        companion object{
-            private const val PERMISSIONS_REQUEST_READ_CONTACT=22
+            cursor.close()
         }
 
+        return contactNameList
+    }
 
+    companion object {
+        private const val PERMISSIONS_REQUEST_READ_CONTACT = 22
+    }
 
 
     override fun onRequestPermissionsResult(
-        requestCode:Int,
-        permissions:Array<out String>,
-        grantResult:IntArray
-    ){
-        super.onRequestPermissionsResult(requestCode,permissions,grantResult)
-        if(grantResult.isNotEmpty()&&grantResult[0]==PackageManager.PERMISSION_GRANTED && requestCode==PERMISSIONS_REQUEST_READ_CONTACT){
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResult: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResult)
+        if (grantResult.isNotEmpty() && grantResult[0] == PackageManager.PERMISSION_GRANTED && requestCode == PERMISSIONS_REQUEST_READ_CONTACT) {
             displayContact()
-        }
-        else{
-            Toast.makeText(this,"Permission refusée par l'utilisateur",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Permission refusée par l'utilisateur", Toast.LENGTH_LONG).show()
         }
     }
-
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode==RESULT_OK){
+        if (resultCode == RESULT_OK) {
             try {
                 val imageUri: Uri = data?.data!!
-                val imageStream=contentResolver.openInputStream(imageUri)
-                val selectImage=BitmapFactory.decodeStream(imageStream)
+                val imageStream = contentResolver.openInputStream(imageUri)
+                val selectImage = BitmapFactory.decodeStream(imageStream)
                 myButtonPicture.setImageBitmap(selectImage)
-            }
-            catch (e:FileNotFoundException){
+            } catch (e: FileNotFoundException) {
                 e.printStackTrace()
-                Toast.makeText(applicationContext,"Une erreur s'est produite",Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, "Une erreur s'est produite", Toast.LENGTH_LONG)
+                    .show()
             }
 
-        }
-        else{
-            Toast.makeText(applicationContext,"Vous n'avez pas choisi d'image",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(applicationContext, "Vous n'avez pas choisi d'image", Toast.LENGTH_LONG)
+                .show()
         }
 
     }
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
--- app/src/main/java/fr/isen/kelly/androidtoolbox/NoteActivity.kt
+++ app/src/main/java/fr/isen/kelly/androidtoolbox/NoteActivity.kt
@@ -8,37 +8,34 @@ class NoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_permission)
+        setContentView(R.layout.activity_permission)}}
+        //fun CameraGalleryManager(){
+//val pictureDialog=AlertDialog.Builder(this)
+//pictureDialog.setTitle("Que faire:")
+//val pictureDialogItems=arrayOf("Prendre une photo","importer depuis la galerie")
+//pictureDialog.setItems(
+//    pictureDialogItems
+//){
+//    _,which ->
+//    when(which){
+//        0-> openCamera()
+//        1-> openGalerie()
 
+//private fun openCamera(){
+//val values=ContentValues()
+//image_uri=contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+//val cameraIntent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//cameraIntent.putExtra(MediaStore....
 
-        //recyclerView
-    }
-    /*fun CameraGalleryManager(){
-        val pictureDialog=AlertDialog.Builder(this)
-        pictureDialog.setTitle("Que faire:")
-        val pictureDialogItems=arrayOf("Prendre une photo","importer depuis la galerie")
-        pictureDialog.setItems(
-            pictureDialogItems
-        ){
-            _,which ->
-            when(which){
-                0-> openCamera()
-                1-> openGalerie()
-            }
-
-        }
-    }
-    private fun openCamera(){
-    val values=ContentValues()
-    image_uri=contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
-    val cameraIntent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-    cameraIntent.putExtra(MediaStore....
-
-
-    }*/
-
-
+// override fun selection() {
+// val items=arrayOf<CharSequence>("Take photo","Choose from gallery","Cancel")
+// val builder=AlertDialog.Builder(this)
+// builder.setItems(items) { dialog, item -> if(items[item] == "Take photo"){
+// requestStoragePermission(true)
+// } else if(items[item]=="Choose from gallery"){
+// requestStoragePermission(false)
+// } else if(items[item]=="Cancel"){
+// dialog.dismiss()
 
 
 
-}
