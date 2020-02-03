0bdd199058cd9a618d7f3a6817472a4367bb7fa7
ChestnutGum <kelly.boutemeur@isen.yncrea.fr>
2020-02-03 17:35:47 +0100

    tests api (début) suite

--- app/src/main/java/fr/isen/kelly/androidtoolbox/UsersAdapter.kt
+++ app/src/main/java/fr/isen/kelly/androidtoolbox/UsersAdapter.kt
@@ -16,14 +16,20 @@ class UsersAdapter(val users: List<User>) : RecyclerView.Adapter<UsersAdapter.Vi
 
         inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             val nameTextView = itemView.userName
+            val mailTextView = itemView.userMail
+            val addressTextView = itemView.userAddr
         }
 
 
         override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
             val user = users.get(position)
             //viewHolder.nameTextView.text(user.name)
-            val textView = viewHolder.nameTextView
-            textView.text = user.name
+            val textName = viewHolder.nameTextView
+            val textMail=viewHolder.mailTextView
+            val textAddr=viewHolder.addressTextView
+            textName.text = user.name
+            textMail.text=user.email
+            textAddr.text=user.adresse
         }
 
         override fun getItemCount(): Int = users.size
